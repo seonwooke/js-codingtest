3f1783fefeb264a579e879ba467942d2411e4ced
seonwooke <seonookkim@gmail.com>
2023-06-14 01:19:19 +0900

    :white_check_mark: Backtracking 15652
    Backtracking 15652

--- backtracking/15652.js
+++ backtracking/15652.js
@@ -1,1 +1,32 @@
 let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0].split(' ')[0]);
+let m = Number(input[0].split(' ')[1]);
+let arr = Array.from({length: n}, (value, index) => (index + 1));
+
+let selected = [];
+let answer = "";
+
+dfs(arr, 0, 0);
+console.log(answer);
+
+function dfs (arr, depth, start) {
+  if (depth == m) {
+    let result = [];
+    for (let i of selected) {
+      result.push(arr[i]);
+    }
+    for (let x of result) {
+      answer += x + " ";
+    }
+    answer += "\n";
+    return;
+  }
+
+  for (let i = start ; i < arr.length ; i++) {
+    selected.push(i);
+    dfs(arr, depth + 1, i);
+    selected.pop();
+  }
+}
